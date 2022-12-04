9fc84864594ada051bcafd179e52a31dd64cbc15
General T <generaltromboni@outlook.com>
2022-12-04 14:51:12 -0500

    Minor changes. Small clock too why not

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,22 @@
-// Theres no javascript for this bitch XD
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML =  `${h + ":" + m + ":" + s} <-- for your time-viewing pleasure :)`;
+    setTimeout(startTime, 1000);
+  }
+  
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i}; 
+    return i;
+  }
+
+
+console.log("Cookies: " + navigator.cookieEnabled);
+console.log("Language: " + navigator.language);
+console.log("Platform: " + navigator.platform);
+console.log("User Agent: " + navigator.userAgent);
+console.log("Webdriver: " + navigator.webdriver);
